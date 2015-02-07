9a3c44ba967fd85a390d32f9247ec6e64a4b0f4f
Rachel Brindle <you@subluminal.net>
2015-02-07 09:21:40 -0800

    Adds parameter and explanation text to docs

--- spec/acceptance/bulb_controller_spec.rb
+++ spec/acceptance/bulb_controller_spec.rb
@@ -8,8 +8,8 @@ resource 'Bulbs' do
     before do
       allow(Huey::Bulb).to receive(:all).and_return([obj])
     end
-    example "Fetching all of the bulbs" do
-      do_request
+    example_request "Fetching all of the bulbs" do
+      explanation "It fetches all of the bulbs and returns them as an array of json objects"
       expect(response_status).to eq(200)
       parsed_body = JSON.parse(response_body)
       expect(parsed_body).to eq([obj])
@@ -22,9 +22,10 @@ resource 'Bulbs' do
       allow(Huey::Bulb).to receive(:find).with('light').and_return(obj)
     end
 
-    parameter :id, "The name (string) or id number (integer) for a given light"
+    parameter :id, "The name (string) or id number (integer) for a given light", :required => true
 
     example "Fetching a single bulb by id number" do
+      explanation "It fetches a single bulb given the bulb's id number (as an integer)"
       do_request(id: 1)
       expect(response_status).to eq(200)
       parsed_body = JSON.parse(response_body)
@@ -32,6 +33,7 @@ resource 'Bulbs' do
     end
 
     example "Fetching a single bulb by name" do
+      explanation "It fetches a single bulb given the bulb's name (as a string)"
       do_request(id: 'light')
       expect(response_status).to eq(200)
       parsed_body = JSON.parse(response_body)
@@ -54,8 +56,18 @@ resource 'Bulbs' do
       allow(Huey::Bulb).to receive(:find).with(1).and_return(bulb)
     end
 
-    example 'Updating the bulb' do
+    parameter :id, "The name (string) or id number (integer) for a given light", :required => true
+    parameter :on, "Indicates whether to turn the bulb on or off, this does not affect connectivity. Expected as a bool (either 'true', or 'false', case insensitive)."
+    parameter :brightness, "Sets the brightness for a bulb, between 0 and 254. Setting brightess to 0 does not turn the bulb off"
+    parameter :hue, "Sets the hue for a bulb, between 0 and 65535. Multiply the hue degree by 182 to get this"
+    parameter :saturation, "Sets the saturation value for the bulb, between 0 and 254"
+    parameter :ct, "Sets the color temperature for the bulb, expressed in mireds (http://en.wikipedia.org/wiki/Mired), is an integer between 154 and 500"
+    parameter :transition_time, "Sets the time to transition from one state to another, 0 is instantaneous. Tenths of a second expressed as integers (e.g. 10 is 1 second, 15 is 1.5 seconds, etc.)"
+    parameter :rgb, "Convenience method, pass in HTML hex values (e.g. '#777', '#FF88FF'. Include preceding '#'), and will automatically convert to hue/saturation"
+
+    example 'Updating a bulb' do
       do_request(id: 1, on: false, brightness: 200, hue: 1024, saturation: 127, ct: 200, transition_time: 0, rgb: "#F30")
+      explanation "It fetches a single bulb given the bulb's name or id number, and updates it with the given values"
 
       expect(bulb).to have_received(:on=).with(false)
       expect(bulb).to have_received(:bri=).with(200)
